80fc4b2db4fd2893834c330b29eedf022235ecc1
Raju3281 <140163937+Raju3281@users.noreply.github.com>
2025-03-07 17:52:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SecretSanta from './SecretSanta';
+
+function App() {
+  return (
+    <div className="App">
+     <SecretSanta/>
+    </div>
+  );
+}
+
+export default App;
